9de24c57ca58ede3c996f8791fd763f171e0aa5f
Ayuna <aunmiku@gmail.com>
2021-04-08 19:38:05 +0300

    styled Navbar

--- client/src/components/Services/Services.js
+++ client/src/components/Services/Services.js
@@ -1,9 +1,22 @@
+// import dogMainPage from '../../../public/assets'
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router";
 
 function Services() {
+
+  const history = useHistory();
+
+  const addOrderHandler = () => {
+
+    history.push('/order');
+  }
+
+
   return (
     <div>
       <h3>Услуги</h3>
-
+      {/* <img src={ dogMainPage }></img> */}
+      <Button onClick={ addOrderHandler }>Заказать выгул</Button>
     </div>
   );
 }
