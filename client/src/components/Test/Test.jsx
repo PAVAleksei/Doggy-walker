d8d63a3129998f933067c60509e4306c993e483a
LeonidRed <nikulin.l.m@gmail.com>
2021-04-11 17:20:04 +0300

    Commit for pull from Dev

--- client/src/components/Test/Test.jsx
+++ client/src/components/Test/Test.jsx
@@ -5,8 +5,10 @@ import 'react-dadata/dist/react-dadata.css'
 function Test() {
 
 	const [value, setValue] = useState();
-	console.log(value);
-
+	// console.log(value.data.geo_lat);
+	// console.log(value.data.geo_lon);
+	// console.log(value.value);
+	
 	return (
 		<AddressSuggestions token="8536f85322589081ac698e1b9d9f1979cbd98e52" value={value} onChange={setValue} />
 	)
