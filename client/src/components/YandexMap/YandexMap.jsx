8a880c9fd4a6eaa8f91ad51d430fe025d4c52118
LeonidRed <nikulin.l.m@gmail.com>
2021-04-13 11:06:24 +0300

    yandex map add

--- client/src/components/YandexMap/YandexMap.jsx
+++ client/src/components/YandexMap/YandexMap.jsx
@@ -1,11 +1,15 @@
 
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import { useDispatch, useSelector } from 'react-redux';
+import { YMaps, Map, Placemark, GeolocationControl, ZoomControl, RouteButton } from 'react-yandex-maps';
+
+const location = YMaps.geolocation
+console.log(location, ' <------- location')
 
 const mapData = {
 	center: [55.751574, 37.573856],
 	zoom: 10,
+
 };
 
 // const mycoordinates = [
@@ -14,14 +18,45 @@ const mapData = {
 // ];
 
 export default function YandexMap() {
-
+	const dispatch = useDispatch()
 	const coordinates = useSelector((state) => state.allOrders)
 	console.log(coordinates);
 
 	return (
-		<YMaps>
+		<YMaps >
 			<Map width='100%' height='300px' defaultState={mapData}>
-				{coordinates.map(coordinate => <Placemark geometry={coordinate.address.coordinates} />)}
+
+				<RouteButton instanceRef={ref => {
+					if (ref) {
+						ref.routePanel.state.set({
+							from: [55.751574, 37.573856],
+							to: [59.9386300, 30.3141300],
+							type: "auto"
+						});
+						// const obj = ref.routePanel.getRouteAsync()
+						// obj.then(function (multiRoute) {
+						// 	multiRoute.model.events.add('requestsuccess', function () {
+						// 		const activeRoute = multiRoute.getActiveRoute()
+						// 		if (activeRoute) {
+						// 			let distance = activeRoute.properties.get('distance')
+						// 			// dispatch(addDistance(trip.id, distance))
+						// 		}
+						// 	})
+						// })
+					}
+				}} options={{ float: 'right' }} />
+
+				{coordinates.map(coordinate => <Placemark
+					geometry={coordinate.address.coordinates}
+					properties={{
+						hintContent: coordinate.address.name,
+						balloonContent: coordinate.description,
+					}}
+					modules={['geoObject.addon.balloon', 'geoObject.addon.hint', 'geocode']}
+
+				/>)}
+				<GeolocationControl options={{ float: 'left' }} />
+				<ZoomControl options={{ float: 'right' }} />
 			</Map>
 		</YMaps>
 	)
